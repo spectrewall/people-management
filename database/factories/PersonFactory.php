014844d70aa4590ed2d4bb4f197b77cbdcdaab91
Wallace Carvalho <wallace.carvalho789@gmail.com>
2023-03-22 23:38:30 -0300

    Refactor: Adjusts PersonFactory

--- database/factories/PersonFactory.php
+++ database/factories/PersonFactory.php
@@ -2,10 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\Address;
+use App\Models\Person;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Person>
+ * @extends Factory<Person>
  */
 class PersonFactory extends Factory
 {
@@ -13,11 +15,17 @@ class PersonFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
+     * @noinspection PhpUndefinedMethodInspection
      */
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'cpf' => $this->faker->cpf(false),
+            'phone' => $this->faker->cellphoneNumber(false, true),
+            'birth_date' => $this->faker->date('Y-m-d', '-100 years'),
+            'address_id' => Address::factory(),
         ];
     }
 }
