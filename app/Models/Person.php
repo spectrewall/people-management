58605075a63315038918cd1c8024a1e078ba9b53
Wallace Carvalho <wallace.carvalho789@gmail.com>
2023-03-22 23:36:10 -0300

    Feat: Add $fillable attribute, rules method and relation with Address to Person model

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Person extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'cpf',
+        'phone',
+        'birth_date',
+        'address_id',
+    ];
+
+    public function address(): BelongsTo
+    {
+        return $this->belongsTo(Address::class);
+    }
+
+    public static function rules(): array
+    {
+        return [
+            'name' => 'string|max:255|min:2',
+            'email' => 'string|email|max:255|unique:people',
+            'cpf' => 'required|string|size:11|unique:people',
+            'phone' => 'required|string|max:11|min:10',
+            'birth_date' => 'required|date_format:Y-m-d|before_or_equal:today'
+        ];
+    }
 }
