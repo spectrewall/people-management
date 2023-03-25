4ea98ce94e240e8ad226e7923284c5d099127e10
Wallace Carvalho <wallace.carvalho789@gmail.com>
2023-03-25 00:19:35 -0300

    Feat: Creates input-label custom component

---
+++ resources/views/components/custom/input-label.blade.php
@@ -0,0 +1,11 @@
+@props([
+    'value',
+    'for'
+])
+
+<label
+    {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}
+    for="{{ $for }}"
+>
+    {{ $value }}
+</label>
