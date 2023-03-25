8129284d85fc2e5cea547a330e81a3b5f815fc1f
Wallace Carvalho <wallace.carvalho789@gmail.com>
2023-03-25 18:23:59 -0300

    Feat: Creates error alert in alerts custom component

--- resources/views/components/custom/alerts.blade.php
+++ resources/views/components/custom/alerts.blade.php
@@ -4,3 +4,10 @@
         <p>{{ Session::get('success') }}</p>
     </div>
 @endif
+
+@if (Session::has('error'))
+    <div class="flex items-center bg-red-500 text-white text-sm font-bold px-4 py-3" role="alert">
+        <span class="material-symbols-outlined"> error </span>
+        <p>{{ Session::get('error') }}</p>
+    </div>
+@endif
