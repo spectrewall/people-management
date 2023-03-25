251258368b766072e41690e324b408eb90b6d9aa
Wallace Carvalho <wallace.carvalho789@gmail.com>
2023-03-25 13:22:56 -0300

    Feat: Creates AddressService with update method

---
+++ app/Services/AddressService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Address;
+use App\Repository\Interfaces\AddressRepositoryInterface;
+use Illuminate\Support\Facades\DB;
+use Throwable;
+
+class AddressService
+{
+    public function __construct(
+        private readonly AddressRepositoryInterface $addressRepository
+    ) {
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param array $attributes
+     * @param int|string|Address $address
+     * @return Address
+     * @throws Throwable
+     */
+    public function update(array $attributes, int|string|Address $address): Address
+    {
+        $address = $address instanceof Address ? $address :
+            $this->addressRepository->findOrFail(intval($address));
+
+        return DB::transaction(function () use ($address, $attributes) {
+            return $this->addressRepository->update($attributes, $address);
+        });
+    }
+}
