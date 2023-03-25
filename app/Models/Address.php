de1954e574ded074bef74f72a2de4f87eaf49d5f
Wallace Carvalho <wallace.carvalho789@gmail.com>
2023-03-25 12:47:03 -0300

    Feat: Add ide-helper package to project and generate DocBlocks

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Address
+ *
+ * @property int $id
+ * @property string $street
+ * @property string $city
+ * @property string $state
+ * @property string|null $number
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\AddressFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Address newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Address newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Address query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereState($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereStreet($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Address extends Model
 {
     use HasFactory;
--- app/Models/Person.php
+++ app/Models/Person.php
@@ -6,6 +6,34 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * App\Models\Person
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $cpf
+ * @property string $phone
+ * @property string $birth_date
+ * @property int $address_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Address $address
+ * @method static \Database\Factories\PersonFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Person newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Person newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Person query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereAddressId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereBirthDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereCpf($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Person extends Model
 {
     use HasFactory;
